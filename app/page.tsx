dd808f8c388a9d672d5beb2ede64733e0ef95107
vishal911m <vishal911m@gmail.com>
2025-12-24 12:55:07 +0530

    Animating the dialog with css

--- app/page.tsx
+++ app/page.tsx
@@ -40,8 +40,10 @@ function ContactCard({contact}: {contact: Contact}) {
           <Pencil1Icon />
         </Dialog.Trigger>
         <Dialog.Portal>
-          <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-          <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow">
+          <Dialog.Overlay className="fixed inset-0 bg-black/50
+          data-[state=open]:animate-[dialog-overlay-show_200ms] data-[state=closed]:animate-[dialog-overlay-hide_200ms]" />
+          <Dialog.Content className="data-[state=open]:animate-[dialog-content-show_200ms] data-[state=closed]:animate-[dialog-content-hide_200ms] 
+          fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow">
             <div className="flex items-center justify-between">
               <Dialog.Title className="text-xl">
                 Edit contact
