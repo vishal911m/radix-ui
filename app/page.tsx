26d947d6954e460ba748f9fe24cf5a0924997889
vishal911m <vishal911m@gmail.com>
2025-12-30 12:34:12 +0530

    Resetting the form's state after submission

--- app/page.tsx
+++ app/page.tsx
@@ -23,18 +23,6 @@ export default function Page() {
 function ContactCard({contact}: {contact: Contact}){
   let {updateContact} = useContacts();
   let [open, setOpen] = useState(false);
-  const [saving, setSaving] = useState(false);
-
-  const handleSubmit = async(event: FormEvent<HTMLFormElement>)=>{
-    event.preventDefault();
-    setSaving(true);
-
-    let data = Object.fromEntries(new FormData(event.currentTarget));
-    console.log(data);
-
-    await updateContact(contact.id, data);
-    setOpen(false);
-  }
 
   return (
     <div
@@ -63,24 +51,7 @@ function ContactCard({contact}: {contact: Contact}){
                 </Dialog.Close>
               </div>
 
-              <form onSubmit={handleSubmit}>
-                <fieldset disabled={saving} className="group">  
-                  <div className="mt-8 group-disabled:opacity-50">
-                    <ContactFields contact={contact} />
-                  </div>
-                  <div className="mt-8 space-x-6 text-right">
-                    <Dialog.Close className="rounded px-4 py-2 text-sm font-medium text-gray-500 hover:text-gray-600">
-                      Cancel
-                    </Dialog.Close>
-                    <button className="inline-flex justify-center items-center rounded bg-green-500 px-4 py-2 text-sm font-medium 
-                    text-white hover:bg-green-600 group-disabled:pointer-events-none">
-                      <Spinner className="absolute h-4 group-enabled:opacity-0"/>
-                      <span className="group-disabled:opacity-0">Save</span>
-                    </button>
-                  </div>
-                </fieldset>
-              </form>
-
+              <ContactForm contact={contact} afterSave={()=>setOpen(false)}/>
             </Dialog.Content>
           </Dialog.Portal>
         </Dialog.Root>
@@ -89,46 +60,71 @@ function ContactCard({contact}: {contact: Contact}){
   );
 }
 
-function ContactFields({
-  contact,
-}: {
-  contact: Contact
-}) {
-  return (
-    <div className="space-y-6">
-      <div>
-        <label className="text-sm font-medium text-gray-900">Name</label>
-        <input
-          autoFocus
-          className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
-          type="text"
-          defaultValue={contact.name}
-          name="name"
-        />
-      </div>
+function ContactForm({contact, afterSave}: {contact: Contact, afterSave: ()=>void}) {
+  const [saving, setSaving] = useState(false);
+  let {updateContact} = useContacts();
 
-      <div>
-        <label className="text-sm font-medium leading-6 text-gray-900">
-          Role
-        </label>
-        <input
-          className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
-          type="text"
-          defaultValue={contact.role}
-          name="role"
-        />
-      </div>
-      <div>
-        <label className="text-sm font-medium leading-6 text-gray-900">
-          Email address
-        </label>
-        <input
-          className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
-          type="text"
-          defaultValue={contact.email}
-          name="email"
-        />
-      </div>
-    </div>
+  const handleSubmit = async(event: FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    setSaving(true);
+
+    let data = Object.fromEntries(new FormData(event.currentTarget));
+    console.log(data);
+
+    await updateContact(contact.id, data);
+    afterSave();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset disabled={saving} className="group">  
+        <div className="mt-8 group-disabled:opacity-50">
+          <div className="space-y-6">
+            <div>
+              <label className="text-sm font-medium text-gray-900">Name</label>
+              <input
+                autoFocus
+                className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
+                type="text"
+                defaultValue={contact.name}
+                name="name"
+              />
+            </div>
+
+            <div>
+              <label className="text-sm font-medium leading-6 text-gray-900">
+                Role
+              </label>
+              <input
+                className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
+                type="text"
+                defaultValue={contact.role}
+                name="role"
+              />
+            </div>
+            <div>
+              <label className="text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <input
+                className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
+                type="text"
+                defaultValue={contact.email}
+                name="email"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 space-x-6 text-right">
+          <Dialog.Close className="rounded px-4 py-2 text-sm font-medium text-gray-500 hover:text-gray-600">
+            Cancel
+          </Dialog.Close>
+          <button className="inline-flex justify-center items-center rounded bg-green-500 px-4 py-2 text-sm font-medium 
+          text-white hover:bg-green-600 group-disabled:pointer-events-none">
+            <Spinner className="absolute h-4 group-enabled:opacity-0"/>
+            <span className="group-disabled:opacity-0">Save</span>
+          </button>
+        </div>
+      </fieldset>
+    </form>
   );
 }
