169179983ebc818f7043fab08e006f76abaf866b
vishal911m <vishal911m@gmail.com>
2026-01-08 17:52:36 +0530

    Populating Modal

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,15 @@ export default function Page() {
           <Button icon={<QuestionMarkCircledIcon />}>About</Button>
           </Modal.Button>
           <Modal.Content title="About Trello">
-            Some copy
+            <div className="mt-4 space-y-3 text-gray-600">
+              <p>This is a React app built with Radix UI!</p>
+              <p>Technologies used:</p>
+              <ul className="list-disc pl-4">
+                <li>Radix UI Dialog</li>
+                <li>Next.js</li>
+                <li>Tailwind CSS</li>
+              </ul>
+            </div>
           </Modal.Content>
         </Modal>
       </header>
