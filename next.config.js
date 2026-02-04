495d4e6165e49eb8b72e45b942f27250701be083
vishal911m <vishal911m@gmail.com>
2026-02-04 17:02:06 +0530

    Disable Jekyll and deploy static Next.js site

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/radix-ui/",
-  images: {
-    unoptimized: true,
-  },
-  reactStrictMode: true,
-};
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: "export",
+    basePath: "/radix-ui",
+    images: {
+      unoptimized: true,
+    },
+    reactStrictMode: true,
+  };
 
-module.exports = nextConfig;
+  module.exports = nextConfig;
